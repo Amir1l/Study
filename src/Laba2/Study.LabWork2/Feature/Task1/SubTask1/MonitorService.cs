4ed5d670aad638db2d638f1aa10a153da0f9b0c7
amiril <egor-olenev@rambler.ru>
2026-05-29 01:15:26 +0300

    [Feature] Многопоточный счетчик простых чисел

--- src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/MonitorService.cs
+++ src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/MonitorService.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1.DtoModels;
 
@@ -8,7 +9,63 @@ namespace Study.LabWork2.Feature.Task1.SubTask1;
 /// </summary>
 public sealed class MonitorService : IPrimeCounter
 {
-    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount) => throw new NotImplementedException();
+    private readonly object locker = new object();
+    private int primeCount = 0;
+    private List<int> foundPrimes;
 
-    public string GetVersionName() => throw new NotImplementedException();
+    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount)
+    {
+        primeCount = 0;
+        foundPrimes = new List<int>();
+
+        var stopWatch = Stopwatch.StartNew();
+
+        var ranges = PrimeCounter.SplitRange(start, end, threadCount);
+        var threads = new List<Thread>();
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            int threadNumber = i + 1;
+            var range = ranges[i];
+            var thread = new Thread(() => Work(threadNumber, range.start, range.end));
+            thread.Start();
+            threads.Add(thread);
+        }
+
+        foreach (var t in threads) t.Join();
+
+        stopWatch.Stop();
+
+        return new PrimeCountResultDto
+        {
+            PrimeCount = primeCount,
+            ExecutionTime = stopWatch.Elapsed,
+            ThreadCount = threadCount,
+            SynchronizationType = GetVersionName(),
+            FoundPrimes = foundPrimes,
+        };
+    }
+
+    private void Work(int threadNumber, int start, int end)
+    {
+        for (int num = start; num <= end; num++)
+        {
+            Console.WriteLine($"Поток {threadNumber}: обрабатывает число {num}");
+
+            if (PrimeCounter.IsPrime(num))
+            {
+                lock (locker)
+                {
+                    primeCount++;
+                    foundPrimes.Add(num);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Поток {threadNumber}: Нашел простое число {num}");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    public string GetVersionName() => "Monitor service";
 }
--- src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/SemaphoreService.cs
+++ src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/SemaphoreService.cs
@@ -1,14 +1,77 @@
+using System.Diagnostics;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1.DtoModels;
 
 namespace Study.LabWork2.Feature.Task1.SubTask1;
 
 /// <summary>
-/// Версия 3. Использует Semaphore для синхронизации
+/// Версия 3. Использует бинарный Semaphore для синхронизации
 /// </summary>
 public sealed class SemaphoreService : IPrimeCounter
 {
-    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount) => throw new NotImplementedException();
+    // Бинарный семафор (один слот) для взаимного исключения
+    private readonly Semaphore semaphore = new Semaphore(1, 1);
+    private int primeCount = 0;
+    private List<int> foundPrimes;
 
-    public string GetVersionName() => throw new NotImplementedException();
+    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount)
+    {
+        primeCount = 0;
+        foundPrimes = new List<int>();
+
+        var stopWatch = Stopwatch.StartNew();
+
+        var ranges = PrimeCounter.SplitRange(start, end, threadCount);
+        var threads = new List<Thread>();
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            int threadNumber = i + 1;
+            var range = ranges[i];
+            var thread = new Thread(() => Work(threadNumber, range.start, range.end));
+            thread.Start();
+            threads.Add(thread);
+        }
+
+        foreach (var t in threads) t.Join();
+
+        stopWatch.Stop();
+
+        return new PrimeCountResultDto
+        {
+            PrimeCount = primeCount,
+            ExecutionTime = stopWatch.Elapsed,
+            ThreadCount = threadCount,
+            SynchronizationType = GetVersionName(),
+            FoundPrimes = foundPrimes,
+        };
+    }
+
+    private void Work(int threadNumber, int start, int end)
+    {
+        for (int num = start; num <= end; num++)
+        {
+            Console.WriteLine($"Поток {threadNumber}: обрабатывает число {num}");
+
+            if (PrimeCounter.IsPrime(num))
+            {
+                semaphore.WaitOne();
+                try
+                {
+                    primeCount++;
+                    foundPrimes.Add(num);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Поток {threadNumber}: Нашел простое число {num}");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    public string GetVersionName() => "Semaphore Service";
 }
